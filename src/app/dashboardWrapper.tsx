f765ef01f12e4be8b2c72b062116e97ab894a482
Tan Chu <tan.winh@gmail.com>
2024-09-26 21:53:09 +0200

    test

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -6,8 +6,7 @@ const DashboardWrapper = ({children} : {children: React.ReactNode}) => {
   return (
     <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
         {/*sidebar*/}
-        sidebar
-
+        
         <main 
             className={'dark:bg-dark-bg flex w-full flex-col bg-gray-50  md:pl-64'}
         >
