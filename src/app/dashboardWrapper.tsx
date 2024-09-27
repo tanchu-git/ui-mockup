8f9becb4243b6603d6b5d74fc679d1579b701e7c
Tan Chu <tan.chu@outlook.com>
2024-09-27 10:16:41 +0200

    Search bar

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import Navbar from "@/app/(components)/Navbar";
 
 type Props = {}
 
 const DashboardWrapper = ({children} : {children: React.ReactNode}) => {
   return (
+    // Sidebar navigation
     <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
-        {/*sidebar*/}
-        
+        {}        
         <main 
+            // Top navigation bar
             className={'dark:bg-dark-bg flex w-full flex-col bg-gray-50  md:pl-64'}
         >
-            {/*navbar*/}
+            <Navbar />
             
             {children}
         </main>
