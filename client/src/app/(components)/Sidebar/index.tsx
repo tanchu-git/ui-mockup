0ad92d2fe2d54d652c248c3db517c3bd822c0a7d
Tan Chu <tan.winh@gmail.com>
2024-09-28 21:00:34 +0200

    comment

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -27,7 +27,7 @@ const Sidebar = () => {
       <div className='flex h-[100%] w-full flex-col justify-start'>
         {/* Top logo */}
         <div className='z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black'>
-          {/* Name */}
+          {/* Top left name */}
           <div className='text-xl font-bold text-gray-800 dark:text-white'>
             BIPCARDS
           </div>
