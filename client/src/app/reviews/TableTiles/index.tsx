472682f7e4fd78e7b29724d65001f5ca4168ab9c
Tan Chu <tan.winh@gmail.com>
2024-10-01 23:14:39 +0200

    grid2

--- client/src/app/reviews/TableTiles/index.tsx
+++ client/src/app/reviews/TableTiles/index.tsx
@@ -2,6 +2,7 @@ import { useAppSelector } from "@/app/redux";
 import Header from "@/components/Header";
 import { dataGridClassNames, dataGridSxStyles, sxCustomStyling } from "@/lib/utils";
 import { useGetReviewsQuery } from "@/state/api";
+import { Grid2, Stack } from "@mui/material";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import React from "react";
 
@@ -25,6 +26,30 @@ const fourStarColumns: GridColDef[] = [
   },
 ];
 
+const threeStarColumns: GridColDef[] = [
+  {
+    field: "review",
+    headerName: "3 Star Reviews",
+    width: 400,
+  },
+];
+
+const twoStarColumns: GridColDef[] = [
+  {
+    field: "review",
+    headerName: "2 Star Reviews",
+    width: 400,
+  },
+];
+
+const oneStarColumns: GridColDef[] = [
+  {
+    field: "review",
+    headerName: "1 Star Reviews",
+    width: 400,
+  },
+];
+
 const TableTilesView = ({ id }: Props) => {
   const darkMode = useAppSelector((state) => state.global.darkMode);
   const {
@@ -38,26 +63,52 @@ const TableTilesView = ({ id }: Props) => {
 
   const fiveStarReviews = reviews.filter((review) => review.score === 5)
   const fourStarReviews = reviews.filter((review) => review.score === 4)
+  const threeStarReviews = reviews.filter((review) => review.score === 3)
+  const twoStarReviews = reviews.filter((review) => review.score === 2)
+  const oneStarReviews = reviews.filter((review) => review.score === 1)
 
   return (
-    <div className="h-full w-[450px] px-4 pb-8 xl:px-6">
+    <div className="h-full w-full px-4 pb-8 xl:px-6">
       <h1 className="me-2 text-lg font-bold dark:text-white py-5">
         Reviews
       </h1>
-      <DataGrid
-        autoHeight    
-        rows={fiveStarReviews || []}
-        columns={fiveStarColumns}
-        className={"border-hidden bg-white shadow dark:border-stroke-dark dark:bg-dark-secondary dark:text-gray-200"}
-        sx={sxCustomStyling(darkMode)}
-      />
-      <DataGrid
-        autoHeight
-        rows={fourStarReviews || []}
-        columns={fourStarColumns}
-        className="border-transparent bg-white shadow dark:border-stroke-dark dark:bg-dark-secondary dark:text-gray-200"
-        sx={sxCustomStyling(darkMode)}
-      />
+      <Grid2 container spacing={2}>
+        <DataGrid
+          autoHeight    
+          rows={fiveStarReviews || []}
+          columns={fiveStarColumns}
+          className={"border-hidden bg-white shadow dark:border-stroke-dark dark:bg-dark-secondary dark:text-gray-200"}
+          sx={sxCustomStyling(darkMode)}
+        />
+        <DataGrid
+          autoHeight
+          rows={fourStarReviews || []}
+          columns={fourStarColumns}
+          className="border-transparent bg-white shadow dark:border-stroke-dark dark:bg-dark-secondary dark:text-gray-200"
+          sx={sxCustomStyling(darkMode)}
+        />
+        <DataGrid
+          autoHeight
+          rows={threeStarReviews || []}
+          columns={threeStarColumns}
+          className="border-transparent bg-white shadow dark:border-stroke-dark dark:bg-dark-secondary dark:text-gray-200"
+          sx={sxCustomStyling(darkMode)}
+        />
+        <DataGrid
+          autoHeight
+          rows={twoStarReviews || []}
+          columns={twoStarColumns}
+          className="border-transparent bg-white shadow dark:border-stroke-dark dark:bg-dark-secondary dark:text-gray-200"
+          sx={sxCustomStyling(darkMode)}
+        />
+        <DataGrid
+          autoHeight
+          rows={oneStarReviews || []}
+          columns={oneStarColumns}
+          className="border-transparent bg-white shadow dark:border-stroke-dark dark:bg-dark-secondary dark:text-gray-200"
+          sx={sxCustomStyling(darkMode)}
+        />
+      </Grid2>
     </div>
   );
 };
