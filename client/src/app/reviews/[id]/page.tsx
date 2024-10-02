89692aa856b780656dae33c41f5a8bd2c1433bed
Tan Chu <tan.winh@gmail.com>
2024-10-02 17:39:10 +0200

    changed name

--- client/src/app/reviews/[id]/page.tsx
+++ client/src/app/reviews/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react'
 import ImproveHeader from "@/app/improve_rank/ImproveHeader";
-import Tile from '../TableTiles';
+import TileRevies from '../TableTiles';
 
 type Props = {
   params: {id: string}
@@ -14,7 +14,7 @@ const Reviews = ({params}: Props) => {
   // Modal is for adding a 'Add Task' functionality later
   return (
     <div>
-        <Tile id={id}/>
+        <TileRevies id={id}/>
     </div>
   )
 }
