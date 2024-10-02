9f168506093f2686835cac629ce71192d47781f1
Tan Chu <tan.chu@outlook.com>
2024-10-02 11:05:38 +0200

    overview

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
+import Overview from "./overview/page";
 
 export default function Home() {
   return (    
-    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      Hi
-    </main>
+    <Overview/>
   );
 }
