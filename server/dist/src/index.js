2a7d98c47ae2387b620c6f651020f5dcf698d773
Tan Chu <tan.chu@outlook.com>
2024-10-03 09:57:07 +0200

    server config for amazon

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -34,7 +34,7 @@ app.use("/rankData", rankDataRoute_1.default);
 app.use("/reviews", reviewsRoute_1.default);
 app.use("/tasks", taskRoute_1.default);
 // Server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3000;
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server running on port: ${port}`);
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -34,8 +34,8 @@ app.use("/reviews", reviewsRoute)
 app.use("/tasks", taskRoute)
 
 // Server
-const port = process.env.PORT || 3000;
+const port = Number(process.env.PORT) || 3000;
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server running on port: ${port}`);
 })
