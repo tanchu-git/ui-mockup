7647c204ec4eed6307ed1a38a62c19a6d1662fcf
Tan Chu <tan.winh@gmail.com>
2024-10-25 20:09:38 +0200

    link done

--- feedback/src/app/[feedbackLink]/page.tsx
+++ feedback/src/app/[feedbackLink]/page.tsx
@@ -3,21 +3,10 @@
 import { useGetFeedbackLinkQuery } from "@/state/api";
 import { FeedbackView } from "../framerVariant";
 
-export default function Page({ params }: { params: { feedbackLink: string } }) {
-    const {
-        data: business,
-        isLoading: businessLoading,
-        isError: businessError
-      } = useGetFeedbackLinkQuery({ feedbackLink: params.feedbackLink });
-
-    if (businessLoading) return <div>Loading..</div>;
-    if (businessError || !business || !business[0]) return <div>{params.feedbackLink} - No such link</div>;
-    
+export default function Page({ params }: { params: { feedbackLink: string } }) {    
     return (
         <div className="">
-            {/* {business[0].placeId}
-            {business[0].ratingLimit} */}
-            <FeedbackView />  
+            <FeedbackView feedbackLink={params.feedbackLink} />  
         </div>
     )
 }
--- feedback/src/app/framerVariant.tsx
+++ feedback/src/app/framerVariant.tsx
@@ -2,6 +2,8 @@ import { useRef, useState } from "react";
 import { motion, useCycle } from "framer-motion";
 import { Rating } from "@mui/material";
 import React from "react";
+import { useGetFeedbackLinkQuery } from "@/state/api";
+import { permanentRedirect, redirect, useRouter } from "next/navigation";
 
 const negFeedback = {
     open: (height = 1000) => ({
@@ -23,13 +25,28 @@ const negFeedback = {
     }
 };
 
-export const FeedbackView = () => {
+type Props = {
+    feedbackLink: string
+}
+
+export const FeedbackView = ( { feedbackLink }: Props ) => {
     const [isOpen, toggleOpen] = useCycle(false, true);
     const containerRef = useRef(null);
 
-    const [value, setValue] = React.useState<number | null>(2);
-    const goodRating = value! >= 4;
+    const [value, setValue] = React.useState<number | null>(2);    
 
+    const {
+        data: business,
+        isLoading: businessLoading,
+        isError: businessError
+      } = useGetFeedbackLinkQuery({ feedbackLink: feedbackLink });
+
+    if (businessLoading) return <div>Loading..</div>;
+    if (businessError || !business || !business[0]) return <div>{feedbackLink} - No such link</div>;
+
+    const goodRating = value! >= Number(business[0].ratingLimit);
+    const placId = business[0].placeId;
+    const router = useRouter()
 
     return (
         <motion.nav
@@ -48,10 +65,16 @@ export const FeedbackView = () => {
                 setValue(newValue);
                 }}
             />
-            <button 
-                onClick={ () => { toggleOpen() }}
+            <button                 
                 className="negButton w-[50px] h-[50px] outline-none border-none 
                     font-bold text-blue-900 text-xl cursor-pointer bg-transparent"
+                    onClick={ () => { 
+                        goodRating 
+                        ? 
+                        router.push(`https://search.google.com/local/writereview?placeid=${placId}`) 
+                        : 
+                        toggleOpen()
+                    }}
             >         
             Go!
             </button>
